1241dd008d5cff49ec7dec6845add3802e9198ed
Spymannn <shanini@adneom.com>
2017-03-11 23:01:08 +0100

    feat(rotation): rotation

--- scripts/cube.js
+++ scripts/cube.js
@@ -1,35 +1,37 @@
 console.log('cube js is loaded!');
 console.log('THREE',THREE);
 
-  AFRAME.registerComponent('scale-on-click', {
+window.onload = init;
+
+
+function init() {
+  const scene = document.querySelector('a-scene');
+
+      if (scene.hasLoaded) {
+        // run();
+      } else {
+        scene.addEventListener('loaded', run);
+      }
+
+      
+}
+
+
+  AFRAME.registerComponent('rotate-on-enter', {
     schema: {
-      to: {default: '2.5 2.5 2.5'}
+      true: {default: true}
     },
     init: function () {
       window.cube=this;
-      var data = this.data;
-      var isAnimationStarted = false;
 
-      this.el.addEventListener('animationstart', function() {
-          isAnimationStarted = true;
-          console.log('animation started!');
-      })
-
-      this.el.addEventListener('animationend', function() {
-        isAnimationStarted = false;
-        // box.emit('letsRotate');        
-        console.log('animation end');     
-      })
-
-      this.el.addEventListener('click', function () {
-        if(!isAnimationStarted) {
-          box.emit('letsRotate');          
-        }
-
-        // box.emit('letsRotate'); 
-            
-        console.log('this : ', this);
+      this.el.addEventListener('mouseenter', function () {
+          this.setAttribute('rotation',this.getAttribute('rotation'));
+          this.emit('letsRotate');          
       });
+
+      this.el.addEventListener('mouseleave', function() {
+        rot_anim.stop();
+      })
     }
   });
 
