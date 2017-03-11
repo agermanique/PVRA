b5c4f73884512c775b6daad930afb635be51bb42
Antoine GERMANIQUE <agermanique@adneom.com>
2017-03-11 22:48:25 +0100

    fixing audio and video on mobile

--- scripts/video.js
+++ scripts/video.js
@@ -51,17 +51,15 @@ function getAverageRGB() {
 }
 
 }
-var video = document.querySelector("#videoElement");
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({ video: true }, handleVideo, videoError);
-}
+// if (navigator.getUserMedia) {
+//     navigator.getUserMedia({ video: true }, handleVideo, videoError);
+// }
 
 function handleVideo(stream) {
 
-    video.src = window.URL.createObjectURL(stream);
 }
 
 function videoError(e) {
