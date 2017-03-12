821651e2126927b395f330361619b8e307be1afc
Antoine GERMANIQUE <agermanique@adneom.com>
2017-03-12 09:46:37 +0100

    hidding button

---
+++ scripts/firebaseRead.js
@@ -0,0 +1,24 @@
+class ScriptRemote {
+
+    constructor() {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyDZd2iv7nI6clDZKhcZWZIlCtrd5ztVJiY",
+            authDomain: "remote-pvra.firebaseapp.com",
+            databaseURL: "https://remote-pvra.firebaseio.com",
+            storageBucket: "remote-pvra.appspot.com",
+            messagingSenderId: "952233990276"
+        };
+
+        firebase.initializeApp(config);
+        // document.querySelector("#letsGo").onclick = this.enterTheVoid;
+        var starCountRef = firebase.database().ref('/isActive');
+        starCountRef.on('value', function (snapshot) {
+            const sky = document.querySelector('a-sky')
+            const assets = document.querySelectorAll('.background')
+            next(sky, assets)
+        });
+    }
+}
+
+new ScriptRemote();
